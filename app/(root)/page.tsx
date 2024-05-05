75a7f7f1c66e4f527740a5297bfd9dc38ec33b7f
egecan12 <egecan18k@gmail.com>
2024-05-05 19:36:41 -0400

    chore: Update dependencies and add Clerk.js for user authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { UserButton } from "@clerk/nextjs";
 
-const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+const Home = () => {
+  return (
+    <div>
+      <p>Home</p>
+      <UserButton afterSignOutUrl="/" />-
+    </div>
+  );
 };
 
-export default ProfilePage;
+export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -21,10 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
